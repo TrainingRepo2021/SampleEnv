131e20aae255d3238278a6446a05e7c9a5967913
Training2021 <gitrainingrepos@gmail.com>
2021-11-17 19:38:41 +0530

    Initial Commit

---
+++ SampleEnvironmentApp/SampleEnvironmentAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleEnvironmentAppApp.swift
+//  SampleEnvironmentApp
+//
+//  Created by Prabhu Patil on 17/11/21.
+//
+
+import SwiftUI
+
+@main
+struct SampleEnvironmentAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
